07c72fa5190876befd47f0f4538fe6926b6b47cd
aashritha-sri <65795802+aashritha-sri@users.noreply.github.com>
2020-05-23 17:08:33 -0700

    Update PreprocessingData.py

--- PreprocessingData.py
+++ PreprocessingData.py
@@ -4,27 +4,32 @@
 # In[3]:
 
 
-# load text
-filename = 'articles1.csv'
+# load data
+filename = 'mental-heath-in-tech-2016.csv'
 file = open(filename, 'rt')
 text = file.read()
 file.close()
 
-# split into words by white space
-words = text.split()
+# split into words
+from nltk.tokenize import word_tokenize
+tokens = word_tokenize(text)
+
+# convert to lower case
+tokens = [w.lower() for w in tokens]
 
 # remove punctuation from each word
 import string
 table = str.maketrans('', '', string.punctuation)
-stripped = [w.translate(table) for w in words]
-print(stripped[:100])
+stripped = [w.translate(table) for w in tokens]
 
-# convert to lower case
-words = [word.lower() for word in words]
+# remove remaining tokens that are not alphabetic
+words = [word for word in stripped if word.isalpha()]
+
+# filter out stop words
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+words = [w for w in words if not w in stop_words]
 print(words[:100])
-
-
-# In[ ]:
 
 
 
